42efd2a0bd0efe362c94378b5add9292c75b8252
-k <slowdive@me.com>
2023-02-02 18:49:57 -0800

    Add basic mechanics

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,27 @@
-use git2::{Cred, Error, RemoteCallbacks};
+use clap::Parser;
+use git2::RemoteCallbacks;
 
-use std::{env, path::Path};
+use std::path::Path;
 
 fn main() {
-    );
+    #[derive(Parser, Debug)]
+    #[command(author, version, about, long_about = None)]
+    struct Args {
+        #[arg(short, long)]
+        url: String,
+        #[arg(short, long)]
+        path: String,
+    }
+
+    let args = Args::parse();
+    let callbacks = RemoteCallbacks::new();
+    let mut options = git2::FetchOptions::new();
+    let mut repo = git2::build::RepoBuilder::new();
+    let builder = repo
+        .bare(true)
+        .remote_create(|repo, name, url| repo.remote_with_fetch(name, url, "+refs/*:refs/*"));
+
+    options.remote_callbacks(callbacks);
+    builder.fetch_options(options);
+    builder.clone(&args.url, Path::new(&args.path)).unwrap();
 }
