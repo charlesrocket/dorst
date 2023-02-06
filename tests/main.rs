b68cc65822090c47a03ec67f8700cd85089a59ab
-k <slowdive@me.com>
2023-02-06 14:22:50 -0800

    Fix `main()`

--- tests/main.rs
+++ tests/main.rs
@@ -1,13 +1,20 @@
-use std::{error::Error, fs, process::Command};
+use std::{error::Error, fs, path::Path, process::Command};
 
 use assert_cmd::prelude::*;
 
 #[test]
 fn main() -> Result<(), Box<dyn Error>> {
     let mut cmd = Command::cargo_bin("dorst")?;
+    let xdg_config_home = std::env::var("XDG_CONFIG_HOME")
+        .unwrap_or(format!("{}/.config", std::env::var("HOME").unwrap()));
+    let file_path = format!("{}/dorst/config.yaml", xdg_config_home);
+
+    if !Path::new(&file_path).exists() {
+        fs::copy("tests/test.yml", file_path)?;
+    }
 
     fs::create_dir_all("punk.dorst")?;
-    cmd.arg("tests/test.yml").assert().success();
+    cmd.assert().success();
 
     Ok(())
 }
