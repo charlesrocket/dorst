4b32d3247bd83afde55c76a3ce60433266f13a06
-k <slowdive@me.com>
2023-02-06 14:39:46 -0800

    Inline path vars

--- tests/main.rs
+++ tests/main.rs
@@ -7,8 +7,8 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut cmd = Command::cargo_bin("dorst")?;
     let xdg_config_home = std::env::var("XDG_CONFIG_HOME")
         .unwrap_or(format!("{}/.config", std::env::var("HOME").unwrap()));
-    let file_path = format!("{}/dorst/config.yaml", xdg_config_home);
-    let conf_path = format!("{}/dorst", xdg_config_home);
+    let file_path = format!("{xdg_config_home}/dorst/config.yaml");
+    let conf_path = format!("{xdg_config_home}/dorst");
 
     if !Path::new(&file_path).exists() {
         fs::create_dir(conf_path)?;
