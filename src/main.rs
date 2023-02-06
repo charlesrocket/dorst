93e87c91dfa321d26e39b7b63051a9d2abd5a18d
-k <slowdive@me.com>
2023-02-06 14:21:26 -0800

    Add SSH key support

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use clap::{value_parser, Arg, ArgAction, ArgMatches, Command};
-use git2::{AutotagOption, RemoteCallbacks};
+use git2::{AutotagOption, Cred, RemoteCallbacks};
 use indicatif::{ProgressBar, ProgressStyle};
 use serde::{Deserialize, Serialize};
 use serde_yaml::{self};
 
 use std::{
     env, fs,
-    io::Write,
+    io::Read,
     path::{Path, PathBuf},
 };
 
@@ -17,9 +17,37 @@ const BANNER: &str = "▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄�
 
 const SPINNER: [&str; 7] = ["░", "▒", "▓", "░", "▒", "▓", "░"];
 
+#[derive(Default)]
+struct Credentials {
+    ssh_password: Option<String>,
+}
+
 #[derive(Debug, Serialize, Deserialize)]
-struct TargetList {
+struct Config {
+    ssh_key: Option<PathBuf>,
+    ssh_pass_protected: Option<bool>,
     targets: Vec<String>,
+}
+
+impl Config {
+    pub fn new() -> Self {
+        let xdg_config_home = std::env::var("XDG_CONFIG_HOME")
+            .unwrap_or(format!("{}/.config", std::env::var("HOME").unwrap()));
+        let file_path = format!("{}/dorst/config.yaml", xdg_config_home);
+
+        let mut file = fs::File::open(file_path).unwrap();
+        let mut contents = String::new();
+
+        file.read_to_string(&mut contents).unwrap();
+
+        let config: Self = serde_yaml::from_str(&contents).unwrap();
+
+        Self {
+            ssh_key: config.ssh_key,
+            ssh_pass_protected: config.ssh_pass_protected,
+            targets: config.targets,
+        }
+    }
 }
 
 fn get_name(target: &str) -> &str {
@@ -38,93 +66,125 @@ fn args() -> ArgMatches {
         .version(env!("CARGO_PKG_VERSION"))
         .help_template("{about-with-newline}Codebase backup utility\n\n{usage-heading} {usage}\n\n{all-args}{after-help}")
         .arg(Arg::new("path")
-            .short('p')
-            .long("path")
             .action(ArgAction::Set)
             .value_name("PATH")
             .help("Backup destination")
             .value_parser(value_parser!(PathBuf))
             .hide_default_value(true)
-            .default_value(get_dir()))
-        .arg(Arg::new("targets")
-            .value_name("TARGETS")
-            .help("Backup targets")
-            .value_parser(value_parser!(PathBuf))
-            .required(true));
+            .default_value(get_dir()));
 
     matches.get_matches()
 }
 
-fn text_prompt(message: &str) -> String {
-    let mut line = String::new();
-    print!("{message}");
-
-    std::io::stdout().flush().unwrap();
-    std::io::stdin()
-        .read_line(&mut line)
-        .expect("Error: Could not read a line");
-
-    line.trim().to_string()
-}
-
-fn bool_prompt(message: &str) -> bool {
-    let mut line = String::new();
-    print!("{message}");
-
-    std::io::stdout().flush().unwrap();
-    std::io::stdin()
-        .read_line(&mut line)
-        .expect("Error: Could not read a line");
-
-    match line.trim().to_lowercase().as_str() {
-        "true" | "yes" | "y" => true,
-        "false" | "no" | "n" => false,
-        _ => {
-            println!("Error: Invalid input, expected 'true' or 'false'");
-            bool_prompt(message)
-        }
+fn pass_prompt(message: &str) -> Option<String> {
+    let pass = rpassword::prompt_password(message);
+    match pass {
+        Ok(pass) => Some(pass),
+        _ => None,
     }
 }
 
-fn pass_prompt(message: &str) -> String {
-    rpassword::prompt_password(format!("{message}")).unwrap()
+fn callbacks(
+    ssh_key: Option<PathBuf>,
+    needs_password: bool,
+    password: Option<String>,
+) -> RemoteCallbacks<'static> {
+    let mut callbacks = RemoteCallbacks::new();
+    callbacks.credentials(move |_url, username_from_url, allowed_types| {
+        if allowed_types.is_ssh_key() {
+            if let Some(ssh_key) = &ssh_key {
+                if needs_password {
+                    if let Some(pwd) = &password {
+                        Cred::ssh_key(username_from_url.unwrap(), None, ssh_key, Some(pwd))
+                    } else {
+                        Cred::default()
+                    }
+                } else {
+                    Cred::ssh_key(username_from_url.unwrap(), None, ssh_key, None)
+                }
+            } else {
+                Cred::default()
+            }
+        } else {
+            Cred::default()
+        }
+    });
+
+    callbacks
+}
+
+fn clone_with_key(
+    key: &Path,
+    destination: &str,
+    target: &str,
+    needs_password: bool,
+    password: Option<String>,
+) {
+    let ssh_key = Path::new(&key);
+    let callbacks = callbacks(Some(ssh_key.into()), needs_password, password);
+    clone(destination, target, callbacks);
+}
+
+fn clone_with_defaults(destination: &str, target: &str) {
+    let callbacks = callbacks(None, false, None);
+    clone(destination, target, callbacks);
+}
+
+fn clone(destination: &str, target: &str, callbacks: RemoteCallbacks) {
+    let mut options = git2::FetchOptions::new();
+    let mut repo = git2::build::RepoBuilder::new();
+    let builder = repo
+        .bare(true)
+        .remote_create(|repo, name, url| repo.remote_with_fetch(name, url, "+refs/*:refs/*"));
+
+    options
+        .remote_callbacks(callbacks)
+        .download_tags(AutotagOption::All);
+
+    builder
+        .fetch_options(options)
+        .clone(target, Path::new(&destination))
+        .unwrap();
 }
 
 fn main() {
     let matches = args();
-    let targets = matches.get_one::<PathBuf>("targets").unwrap();
+    let config = Config::new();
+    let mut needs_pwd = false;
+    let mut creds = Credentials::default();
     let path = matches.get_one::<PathBuf>("path").unwrap();
     let spinner = ProgressBar::new_spinner();
-    let target_file = std::fs::File::open(targets).expect("Could not open target list.");
-    let target_list: TargetList =
-        serde_yaml::from_reader(target_file).expect("Could not read target values.");
+
+    println!("{BANNER}");
+
+    if let Some(pwd) = config.ssh_pass_protected {
+        creds.ssh_password = pass_prompt("Enter \x1b[1mSSH\x1b[0m key password:");
+        needs_pwd = pwd;
+    }
 
     spinner.enable_steady_tick(std::time::Duration::from_millis(90));
     spinner.set_style(ProgressStyle::default_spinner().tick_strings(&SPINNER));
 
-    for target in target_list.targets.iter() {
+    for target in config.targets.iter() {
         let dest = format!("{0}/{1}.dorst", &path.display(), get_name(target));
         if Path::new(&dest).exists() {
             fs::remove_dir_all(&dest).unwrap();
         }
 
         let message = format!("\x1b[36mpulling\x1b[0m \x1b[33m{}\x1b[0m", get_name(target));
-        let callbacks = RemoteCallbacks::new();
-        let mut options = git2::FetchOptions::new();
-        let mut repo = git2::build::RepoBuilder::new();
-        let builder = repo
-            .bare(true)
-            .remote_create(|repo, name, url| repo.remote_with_fetch(name, url, "+refs/*:refs/*"));
 
         spinner.set_message(message);
-        options
-            .remote_callbacks(callbacks)
-            .download_tags(AutotagOption::All);
-
-        builder
-            .fetch_options(options)
-            .clone(target, Path::new(&dest))
-            .unwrap();
+        if let Some(ref ssh_key) = config.ssh_key {
+            clone_with_key(
+                ssh_key,
+                &dest,
+                target,
+                needs_pwd,
+                creds.ssh_password.clone(),
+            );
+        } else {
+            clone_with_defaults(&dest, target);
+        }
     }
 
     spinner.finish_with_message("\x1b[1;32mDONE\x1b[0m");
