284ed59e0103e3ef6bdc6544618ab13cdee3221d
-k <slowdive@me.com>
2023-02-06 21:13:22 -0800

    Set test config location

--- tests/main.rs
+++ tests/main.rs
@@ -1,22 +1,14 @@
-use std::{error::Error, fs, path::Path, process::Command};
+use std::{error::Error, fs, process::Command};
 
 use assert_cmd::prelude::*;
 
 #[test]
 fn main() -> Result<(), Box<dyn Error>> {
     let mut cmd = Command::cargo_bin("dorst")?;
-    let xdg_config_home = std::env::var("XDG_CONFIG_HOME")
-        .unwrap_or(format!("{}/.config", std::env::var("HOME").unwrap()));
-    let file_path = format!("{xdg_config_home}/dorst/config.yaml");
-    let conf_path = format!("{xdg_config_home}/dorst");
-
-    if !Path::new(&file_path).exists() {
-        fs::create_dir(conf_path)?;
-        fs::copy("tests/test.yml", file_path)?;
-    }
 
     fs::create_dir_all("punk.dorst")?;
-    cmd.assert().success();
+
+    cmd.arg("--config").arg("tests/test.yml").assert().success();
 
     Ok(())
 }
