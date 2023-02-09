223b46aa2eb2fda67b7b77172ba35d68abe05550
-k <slowdive@me.com>
2023-02-08 19:11:42 -0800

    Add config tests

--- tests/main.rs
+++ tests/main.rs
@@ -1,7 +1,10 @@
-use std::{error::Error, fs, process::Command};
+use std::{error::Error, fs::create_dir_all, io::Write, process::Command};
 
 use assert_cmd::prelude::*;
 use predicates::str::contains;
+use tempfile::NamedTempFile;
+
+mod files;
 
 // TODO
 // Simulate responses
@@ -10,12 +13,44 @@ use predicates::str::contains;
 fn default() -> Result<(), Box<dyn Error>> {
     let mut cmd = Command::cargo_bin("dorst")?;
 
-    fs::create_dir_all("punk.dorst")?;
+    create_dir_all("punk.dorst")?;
 
     cmd.arg("--config")
         .arg("tests/default.yml")
         .assert()
         .success();
+
+    Ok(())
+}
+
+#[test]
+fn config_ssh() -> Result<(), Box<dyn Error>> {
+    let mut cmd = Command::cargo_bin("dorst")?;
+    let mut config = NamedTempFile::new()?;
+    config.write_all(files::SSH_INVALID)?;
+
+    cmd.arg("--config")
+        .arg(config.path())
+        .assert()
+        .failure()
+        .stderr(contains(
+            "Invalid configuration: Password status is missing",
+        ));
+
+    Ok(())
+}
+
+#[test]
+fn config_ssh_pwd() -> Result<(), Box<dyn Error>> {
+    let mut cmd = Command::cargo_bin("dorst")?;
+    let mut config = NamedTempFile::new()?;
+    config.write_all(files::SSH_INVALID_PWD)?;
+
+    cmd.arg("--config")
+        .arg(config.path())
+        .assert()
+        .failure()
+        .stderr(contains("Invalid configuration: SSH key is missing"));
 
     Ok(())
 }
