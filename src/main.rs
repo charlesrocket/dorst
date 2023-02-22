ef2bade6a4d8f0e76f759bde5e0461e130541265
-k <slowdive@me.com>
2023-02-21 17:43:15 -0800

    Edit `copy_dir()`

--- src/main.rs
+++ src/main.rs
@@ -171,8 +171,8 @@ fn copy_dir(src: &str, dst: &str) -> Result<(), Error> {
     fs::create_dir_all(dst)?;
 
     for file in fs::read_dir(src)? {
-        let file = file?;
-        let path = file.path();
+        let src_file = file?;
+        let path = src_file.path();
 
         if path.is_dir() {
             let sub_dst = format!("{}/{}", dst, path.file_name().unwrap().to_str().unwrap());
