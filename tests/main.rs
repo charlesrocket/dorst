2b4cee02deb20e0186018b5d2ad98b186317810b
-k <slowdive@me.com>
2023-03-16 15:57:55 -0700

    Remove old tests

--- tests/main.rs
+++ tests/main.rs
@@ -5,18 +5,14 @@ use tempfile::NamedTempFile;
 use std::{
     env,
     error::Error,
-    fs::{remove_dir_all, remove_file},
+    fs::remove_dir_all,
     io::Write,
     path::Path,
 };
 
-use crate::{
-    files::{CONFIG_EMPTY, TEST_REPO, TEST_REPO_INVALID},
-    helper::test_setup,
-};
+use crate::files::CONFIG_EMPTY;
 
 mod files;
-mod helper;
 
 #[test]
 fn init() -> Result<(), Box<dyn Error>> {
@@ -35,78 +31,6 @@ fn init() -> Result<(), Box<dyn Error>> {
 
     if Path::new("test-init").exists() {
         remove_dir_all("test-init")?;
-    }
-
-    Ok(())
-}
-
-#[test]
-fn local() -> Result<(), Box<dyn Error>> {
-    test_setup(TEST_REPO, "test-local/testrepo", "test-local");
-
-    let mut clone = Command::cargo_bin("dorst")?;
-    let mut fetch = Command::cargo_bin("dorst")?;
-
-    clone
-        .arg("--config")
-        .arg("test-local/config.yaml")
-        .arg("test-local/local")
-        .assert()
-        .success()
-        .stdout(contains(
-            "COMPLETED\u{1b}[0m \
-             \u{1b}[37m(\u{1b}[0m\u{1b}[1;92m1\u{1b}[0m\u{1b}[37m)\u{1b}[0m",
-        ));
-
-    fetch
-        .arg("--config")
-        .arg("test-local/config.yaml")
-        .arg("test-local/local")
-        .assert()
-        .success()
-        .stdout(contains(
-            "COMPLETED\u{1b}[0m \
-             \u{1b}[37m(\u{1b}[0m\u{1b}[1;92m1\u{1b}[0m\u{1b}[37m)\u{1b}[0m",
-        ));
-
-    remove_dir_all("test-local")?;
-
-    Ok(())
-}
-
-#[test]
-fn bad_refs() -> Result<(), Box<dyn Error>> {
-    test_setup(TEST_REPO_INVALID, "test-bad_refs/badrefs", "test-bad_refs");
-
-    let mut cmd = Command::cargo_bin("dorst")?;
-
-    cmd.arg("--config")
-        .arg("test-bad_refs/config.yaml")
-        .arg("test-bad_refs/bad_refs")
-        .assert()
-        .failure()
-        .stderr(contains("badrefs: corrupted loose reference file"));
-
-    remove_dir_all("test-bad_refs")?;
-
-    Ok(())
-}
-
-#[test]
-fn config_new() -> Result<(), Box<dyn Error>> {
-    if Path::new("test-prompt").is_file() {
-        remove_file("test-prompt")?;
-    }
-
-    let mut cmd = Command::cargo_bin("dorst")?;
-
-    cmd.arg("-c");
-    cmd.arg("test-prompt");
-    cmd.write_stdin("foo?\n");
-    cmd.assert().stderr(contains("unsupported URL protocol"));
-
-    if Path::new("test-prompt").is_file() {
-        remove_file("test-prompt")?;
     }
 
     Ok(())
