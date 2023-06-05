11fc9519af9d786aa8ea75b053b9e2784f3514cb
-k <slowdive@me.com>
2023-06-05 00:27:44 -0700

    test: fix `config_empty()`

--- tests/main.rs
+++ tests/main.rs
@@ -96,7 +96,7 @@ fn config_empty() -> Result<(), Box<dyn Error>> {
         .arg(config.path())
         .assert()
         .failure()
-        .stderr(contains("Error: Failed to read the configuration file"));
+        .stderr(contains("Failed to read the configuration file"));
 
     Ok(())
 }
