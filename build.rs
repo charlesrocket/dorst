692a7cadbf5f096f316e0cbeebc59cb237be8c9b
-k <slowdive@me.com>
2023-06-05 00:26:19 -0700

    build: add custom script

---
+++ build.rs
@@ -0,0 +1,8 @@
+fn main() {
+    #[cfg(feature = "gui")]
+    glib_build_tools::compile_resources(
+        &["src/resources"],
+        "src/resources/resources.gresource.xml",
+        "dorst.gresource",
+    );
+}
