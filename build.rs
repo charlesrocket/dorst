484e3954a94c3acb35da4959b6c492edd79d7643
-k <slowdive@me.com>
2023-07-08 13:43:58 -0700

    build: add build-time information

--- build.rs
+++ build.rs
@@ -1,4 +1,6 @@
 fn main() {
+    built::write_built_file().expect("Failed to acquire build-time information");
+
     #[cfg(feature = "gui")]
     glib_build_tools::compile_resources(
         &["src/resources"],
