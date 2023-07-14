c4232bae562cd776ca912c9693926437f4b02473
-k <slowdive@me.com>
2023-07-14 16:54:54 -0700

    feat: change GUI flag
    `gui` -> `--gui`

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,24 @@ mod git;
 mod util;
 
 fn main() {
-    if std::env::args().count() == 2
-        && std::env::args().last() == Some("gui".to_owned())
-        && cfg!(feature = "gui")
-    {
+    if gui_flag() {
         #[cfg(feature = "gui")]
         gui::start();
+        #[cfg(not(feature = "gui"))]
+        {
+            eprintln!("Error: The GUI feature is disabled.");
+            std::process::exit(1);
+        }
     } else {
         #[cfg(feature = "cli")]
         cli::start();
+        #[cfg(not(feature = "cli"))]
+        {
+            println!("The CLI feature is disabled. Exiting...");
+        }
     }
+}
+
+fn gui_flag() -> bool {
+    std::env::args().any(|arg| arg == "--gui")
 }
