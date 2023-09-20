6a0685ab1381c9609df951890d903ba6474bb921
-k <slowdive@me.com>
2023-09-20 09:20:22 -0700

    build(cargo): update glib command

--- prebuild.rs
+++ prebuild.rs
@@ -1,9 +1,11 @@
+use std::process::Command;
+
 fn main() {
     built::write_built_file().expect("Failed to acquire build-time information");
 
     #[cfg(feature = "gui")]
-    std::process::Command::new("glib-compile-resources")
-        .args(&[
+    Command::new("glib-compile-resources")
+        .args([
             "src/resources/resources.xml",
             "--sourcedir=src/resources",
             "--target=src/resources/dorst.gresource",
