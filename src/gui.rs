4f1644ed2d9167310579ed6cccb2088773d81996
-k <slowdive@me.com>
2024-04-07 14:38:13 -0700

    fix(gui): update `load_css()`

--- src/gui.rs
+++ src/gui.rs
@@ -34,7 +34,7 @@ fn build_ui(app: &Application) {
 
 fn load_css() {
     let provider = CssProvider::new();
-    provider.load_from_data(include_str!("resources/style.css"));
+    provider.load_from_string(include_str!("resources/style.css"));
 
     gtk::style_context_add_provider_for_display(
         &Display::default().expect("Could not connect to a display."),
